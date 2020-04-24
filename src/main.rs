e3cf413925bf39779890838544f5612c418591f2
Theodore Keloglou <zf@sirodoht.com>
2020-04-24 21:41:53 +0100

    add get number of neighbours function

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,91 @@ fn render(_state: State) {
         }
         println!();
     }
+fn get_number_of_neighbours(state: &State, row: usize, col: usize) -> u64 {
+    let mut neighbours: u64 = 0;
+
+    // go clockwise, first top
+    if row > 0 {
+        if state.grid[row - 1][col] {
+            neighbours += 1;
+        }
+    }
+
+    // top-right
+    if row > 0 && col < WIDTH - 1 {
+        if state.grid[row - 1][col + 1] {
+            neighbours += 1;
+        }
+    }
+
+    // right
+    if col < WIDTH - 1 {
+        if state.grid[row][col + 1] {
+            neighbours += 1;
+        }
+    }
+
+    // bottom-right
+    if row < HEIGHT - 1 && col < WIDTH - 1 {
+        if state.grid[row + 1][col + 1] {
+            neighbours += 1;
+        }
+    }
+
+    // bottom
+    if row < HEIGHT - 1 {
+        if state.grid[row + 1][col] {
+            neighbours += 1;
+        }
+    }
+
+    // bottom-left
+    if row < HEIGHT - 1 && col > 0 {
+        if state.grid[row + 1][col - 1] {
+            neighbours += 1;
+        }
+    }
+
+    // left
+    if col > 0 {
+        if state.grid[row][col - 1] {
+            neighbours += 1;
+        }
+    }
+
+    // top-left
+    if row > 0 && col > 0 {
+        if state.grid[row - 1][col - 1] {
+            neighbours += 1;
+        }
+    }
+
+    neighbours
+}
+
+fn evolve(old_state: State) -> State {
+    let mut new_state = State {
+        generation: old_state.generation + 1,
+        grid: old_state.grid.clone(),
+    };
+
+    for row in 0..HEIGHT {
+        for col in 0..WIDTH {
+            let is_live = old_state.grid[row][col];
+            let cell_neighbours = get_number_of_neighbours(&old_state, row, col);
+            if is_live && cell_neighbours < 2 {
+                new_state.grid[row][col] = false;
+            } else if is_live && cell_neighbours < 4 {
+                new_state.grid[row][col] = true;
+            } else if is_live {
+                new_state.grid[row][col] = false;
+            } else if !is_live && cell_neighbours == 3 {
+                new_state.grid[row][col] = true;
+            }
+        }
+    }
+
+    new_state
 }
 
 fn main() {
